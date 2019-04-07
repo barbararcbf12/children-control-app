98a910c7686e4b09c2807e4c1753060b6c8d07da
Barbara Ferreira <barbaraferreira12@gmail.com>
2019-04-08 01:41:13 +0200

    More styling

--- src/components/CheckInPage.js
+++ src/components/CheckInPage.js
@@ -1,33 +1,78 @@
-import React, { Component } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
 
-class CheckInPage extends Component {
-    constructor(props, context) {
-      super(props, context);
-  
-      this.state = {
-        lgShow: false,
-      };
-    }
-  
-    render() {
-      let lgClose = () => this.setState({ lgShow: false });
-  
-      return (
-          <Modal
-            size="lg"
-            show={this.state.lgShow}
-            onHide={lgClose}
-            aria-labelledby="example-modal-sizes-title-lg"
-          >
-            <Modal.Header closeButton>
-              <Modal.Title id="example-modal-sizes-title-lg">
-                Large Modal
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>...</Modal.Body>
-          </Modal>
-      );
-    }
-  }
+const JSX_MODAL = ({ selectedChild, header, checkedIn, onhandleCheckIn, onhandleCheckOut, hadlepickupTime, hour }) => {
 
-  export default CheckInPage;
+  render()
+
+    const { selectedChild, header, checkedIn, onhandleCheckIn, onhandleCheckOut, hadlepickupTime, hour } = this.props;
+
+    const exactTime = (
+      <div className="col-md-5">
+        <div className="hour-day" onClick={() => this.hadlepickupTime(this.state.hour + ":00")}>{this.state.hour + ":00"}</div>
+        <div className="hour-day" onClick={() => this.hadlepickupTime(this.state.hour + ":15")}>{this.state.hour + ":15"}</div>
+        <div className="hour-day" onClick={() => this.hadlepickupTime(this.state.hour + ":30")}>{this.state.hour + ":30"}</div>
+        <div className="hour-day" onClick={() => this.hadlepickupTime(this.state.hour + ":45")}>{this.state.hour + ":45"}</div>
+      </div>
+    );
+    
+    const checkinButton = <button className="btn btn-checkin" onClick={onhandleCheckIn.bind(this, this.state.pickupTime)}>CHECK IN</button>; 
+    const checkoutButton = <button className="btn btn-checkout" onClick={onhandleCheckOut.bind(this)}>CHECK OUT</button>; 
+
+    return(
+      <div className="ui dimmer modals visible active">  
+          <div className="ui standard modal visible active">
+            
+            <div className="select-video" > 
+                {selectedChild && (
+                <div id={selectedChild.childId}>
+                  <div className="title">{selectedChild.name.fullName}</div>
+
+                  <div className='div-col'>
+                    <div className="col-md-4">
+                      <img src={selectedChild.image.large} alt={selectedChild.name.fullName} /> 
+                    </div>
+                  </div>
+
+                  { !checkedIn ?
+                  <div className='div-col' >
+                    <div className="col-md-4" style={{ display: 'inline-block'}}>
+                      <div className="infoText">Choose when {selectedChild.name.firstName} will be picked up: </div>
+                        <div className="col-md-6" style={{ display: 'inline-block', padding: '5px' }}>
+                          <div className="hour-day" onClick={() => this.hadleHourSelected("8")}>8am</div>
+                          <div className="hour-day" onClick={() => this.hadleHourSelected("9")}>9am</div>
+                          <div className="hour-day" onClick={() => this.hadleHourSelected("10")}>10am</div>
+                          <div className="hour-day" onClick={() => this.hadleHourSelected("11")}>11am</div>
+                          <div className="hour-day" onClick={() => this.hadleHourSelected("12")}>12am</div>
+                          <div className="hour-day" onClick={() => this.hadleHourSelected("13")}>1pm</div>
+                          <div className="hour-day" onClick={() => this.hadleHourSelected("14")}>2pm</div>
+                          <div className="hour-day" onClick={() => this.hadleHourSelected("15")}>3pm</div>
+                          <div className="hour-day" onClick={() => this.hadleHourSelected("16")}>4pm</div>
+                        </div>
+                        <div className="col-md-6" style={{ display: 'inline-block', padding: '5px' }}>
+                          { this.state.hour !== '' ? exactTime : null }
+                        </div> 
+                    </div>
+                  </div>: null }
+
+                  <div className='div-col'>
+                    <div className="col-md-4">
+                      { checkedIn ? checkoutButton : this.state.hour !== '' ? checkinButton : null }
+                    </div>
+                  </div>
+                  </div>)}
+              </div>
+              
+          </div>
+        </div>
+    )
+  
+};
+
+
+function CheckInPage(props) {
+  return ReactDOM.createPortal(JSX_MODAL, document.querySelector("#modal"));
+}
+
+
+export default CheckInPage;
--- src/components/ChildrenPage.js
+++ src/components/ChildrenPage.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-
-import CheckInPage from './CheckInPage';
-
-// const ChildrenPage = ({ children, onhandleSelectChild, selectedChild, onhandleCheckIn, onhandleCheckOut }) => { 
 
 class ChildrenPage extends Component {
   constructor() {
     super();
     this.state = { 
       hour: '' ,
-      pickupTime: '',
-      selected: 'false',
-      lgShow: false
+      pickupTime: ''
     };
   }
 
@@ -29,12 +22,6 @@ class ChildrenPage extends Component {
 
     const { children, onhandleSelectChild, selectedChild, onhandleCheckIn, onhandleCheckOut, checkedIn } = this.props;
 
-    let lgClose = () => this.setState({ lgShow: false });
-
-    // const styleselectedChild = (
-    //   children.length !== 0 ? 'block' : 'none'
-    // )
-
     const exactTime = (
       <div className="col-md-5">
         <div className="hour-day" onClick={() => this.hadlepickupTime(this.state.hour + ":00")}>{this.state.hour + ":00"}</div>
@@ -44,54 +31,50 @@ class ChildrenPage extends Component {
       </div>
     );
 
-    const checkinButton = <button onClick={onhandleCheckIn.bind(this, this.state.pickupTime)}>CHECK IN</button>; //<Button variant="success" onClick={onhandleCheckIn.bind(this, this.state.pickupTime)}>CHECK IN</Button>; //
-    const checkoutButton = <button onClick={onhandleCheckOut.bind(this)}>CHECK OUT</button>; //<Button variant="danger" onClick={onhandleCheckOut.bind(this)}>CHECK OUT</Button>; //
+    const checkinButton = <button className="btn btn-checkin" onClick={onhandleCheckIn.bind(this, this.state.pickupTime)}>CHECK IN</button>; 
+    const checkoutButton = <button className="btn btn-checkout" onClick={onhandleCheckOut.bind(this)}>CHECK OUT</button>; 
 
     return(
       <div className="col-md-12">
 
-        {/* <Button onClick={() => this.setState({ lgShow: true })}>
-          Large modal
-        </Button> */}
-        
         <div className="select-video" > 
-        {/* //style={{ display: styleselectedChild }}>  */}
-          {selectedChild && (<div id={selectedChild.childId}>
+          {selectedChild && (
+          <div id={selectedChild.childId}>
             <div className="title">{selectedChild.name.fullName}</div>
+
             <div className='div-col'>
-              <div className="col-md-5">
+              <div className="col-md-4">
                 <img src={selectedChild.image.large} alt={selectedChild.name.fullName} /> 
               </div>
             </div>
 
-            <div className="infoText">Choose when {selectedChild.name.firstName} will be picked up: </div>
-            { !checkedIn ? 
-              <div className="col-md-2" style={{ display: 'inline-block' }}>
-                {/* <div className="label">Birthday: {selectedChild.birthday}</div> */}
-                {/* <div className="col-md-12"> */}
-                  <div className="hour-day" onClick={() => this.hadleHourSelected("8")}>8am</div>
-                  <div className="hour-day" onClick={() => this.hadleHourSelected("9")}>9am</div>
-                  <div className="hour-day" onClick={() => this.hadleHourSelected("10")}>10am</div>
-                  <div className="hour-day" onClick={() => this.hadleHourSelected("11")}>11am</div>
-                  <div className="hour-day" onClick={() => this.hadleHourSelected("12")}>12am</div>
-                  <div className="hour-day" onClick={() => this.hadleHourSelected("13")}>1pm</div>
-                  <div className="hour-day" onClick={() => this.hadleHourSelected("14")}>2pm</div>
-                  <div className="hour-day" onClick={() => this.hadleHourSelected("15")}>3pm</div>
-                  <div className="hour-day" onClick={() => this.hadleHourSelected("16")}>4pm</div>
-                {/* </div> */}
-            </div> : null }
-            { !checkedIn ? 
-                <div className="col-md-2" style={{ display: 'inline-block' }}>
-                {/* <div className="col-md-12"> */}
-                  { this.state.hour !== '' ? exactTime : null }
-                {/* </div> */}
+            { !checkedIn ?
+            <div className='div-col' >
+              <div className="col-md-4" style={{ display: 'inline-block'}}>
+                <div className="infoText">Choose when {selectedChild.name.firstName} will be picked up: {this.state.pickupTime} </div>
+                  <div className="col-md-6" style={{ display: 'inline-block', padding: '5px' }}>
+                    <div className="hour-day" onClick={() => this.hadleHourSelected("8")}>8am</div>
+                    <div className="hour-day" onClick={() => this.hadleHourSelected("9")}>9am</div>
+                    <div className="hour-day" onClick={() => this.hadleHourSelected("10")}>10am</div>
+                    <div className="hour-day" onClick={() => this.hadleHourSelected("11")}>11am</div>
+                    <div className="hour-day" onClick={() => this.hadleHourSelected("12")}>12am</div>
+                    <div className="hour-day" onClick={() => this.hadleHourSelected("13")}>1pm</div>
+                    <div className="hour-day" onClick={() => this.hadleHourSelected("14")}>2pm</div>
+                    <div className="hour-day" onClick={() => this.hadleHourSelected("15")}>3pm</div>
+                    <div className="hour-day" onClick={() => this.hadleHourSelected("16")}>4pm</div>
+                  </div>
+                  <div className="col-md-6" style={{ display: 'inline-block', padding: '5px' }}>
+                    { this.state.hour !== '' ? exactTime : null }
+                  </div> 
+              </div>
             </div>: null }
+
             <div className='div-col'>
-                <div className="col-md-3">
-                  { checkedIn ? checkoutButton : checkinButton }
+              <div className="col-md-4">
+                { checkedIn ? checkoutButton : this.state.hour !== '' ? checkinButton : null }
               </div>
             </div>
-          </div>)}
+            </div>)}
         </div>
          
        <div>
